e9da3dc452bf2a28dfc483b17e87f0fc0a76d721
Costar Mihai <mihai.costar@yahoo.com>
2023-05-21 11:52:48 +0300

    Added DbContext
    Episode #22

---
+++ BlazorEcommerce/Server/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorEcommerce.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
